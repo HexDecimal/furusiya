9b6150b1a24b49ff7bc2b76913ed675062c680c2
nightblade9 <nightbladecodes@gmail.com>
2017-12-06 17:03:50 -0500

    Revert "Start on no-TDL unit tests"
    This reverts commit 06b2de7b3cd4fda7d9367336f647cb98a66c3266.

--- tests/ix/test_map_renderer.py
+++ tests/ix/test_map_renderer.py
@@ -3,7 +3,7 @@ import unittest
 from furusiya.ecs.entity import Entity
 from furusiya.entities.player import Player
 from furusiya.io.map_renderer import MapRenderer
-from tests.mocks.mock_tdl_adapter import MockTdlAdapter
+from furusiya.io.adapters.tdl_adapter import TdlAdapter
 from furusiya.maps.map import Map
 from unittest.mock import MagicMock
 from unittest.mock import patch
@@ -23,13 +23,19 @@ class TestMapRenderer(unittest.TestCase):
         # Player is at (0, 0)
         player = Player()
 
-        
+        # Mock the FOV tiles.
+        light_radius = 5
+        fov_tiles = []        
+
+        for i in range(light_radius):
+            fov_tiles.append((i, 0)) # horizontal ray
+            fov_tiles.append((0, i)) # vertical ray
 
         # Sanity check: it's not explored yet
         for (x, y) in fov_tiles:
             self.assertFalse(map.tiles[x][y].is_explored)
 
-        tdl_adapter = MockTdlAdapter()
+        tdl_adapter = TdlAdapter("Test Window", map_width, map_height)
         tdl_adapter.calculate_fov = MagicMock(return_value = fov_tiles)
 
         renderer = MapRenderer(map, player, tdl_adapter)
